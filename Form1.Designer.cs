2c1f71b6405ea35bea27044a92150413c9a1680c
Zane Marcoe <zmarcoe5840@students.cptc.edu>
2026-04-15 13:41:57 -0700

    Button1 renamed to btnGreeting and changed text attribute

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnGreeting = new Button();
             SuspendLayout();
             // 
-            // button1
+            // btnGreeting
             // 
-            button1.Location = new Point(282, 145);
-            button1.Name = "button1";
-            button1.Size = new Size(146, 64);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnGreeting.Location = new Point(72, 79);
+            btnGreeting.Name = "btnGreeting";
+            btnGreeting.Size = new Size(146, 64);
+            btnGreeting.TabIndex = 0;
+            btnGreeting.Text = "Click This Button";
+            btnGreeting.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(307, 223);
+            Controls.Add(btnGreeting);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -53,6 +53,6 @@
 
         #endregion
 
-        private Button button1;
+        private Button btnGreeting;
     }
 }
